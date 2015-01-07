7fb492b2e9b65b813905e41f4e35525b108e5a13
naokirin <naoki.rin186@gmail.com>
2015-01-08 06:22:50 +0900

    [add][fix]イベント詳細ページを閲覧できる
    適切にEventモデルのインスタンスを生成できていなかったので修正する

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,21 @@
 class EventsController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate, except: :show
 
   def new
     @event = current_user.created_events.build
   end
 
   def create
-    @event = current_user.created_events.build
+    @event = current_user.created_events.build(event_params)
     if @event.save
       redirect_to @event, notice: '作成しました'
     else
       render :new
     end
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 
   private
